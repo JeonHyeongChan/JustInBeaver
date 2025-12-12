9c18af26555bb8846599e3b2efea3a1dcf675935
Juub0618 <dnqls1243@gmail.com>
2025-12-12 12:17:37 +0900

    <feat> 플레이어 테스트용 이동, 점프 구현
    <feat> 플레이어 테스트용 이동, 점프 구현

---
+++ justinbeaver/Assets/01_Scripts/Player/PlayerMovemant.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovemant : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;
+
+    private CharacterController controller;
+    private Vector2 moveInput;
+    private Vector3 velocity;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Move();
+        ApplyGravity();
+    }
+
+    private void Move()
+    {
+        Vector3 moveDir = new Vector3(moveInput.x, 0f, moveInput.y);
+
+        controller.Move((moveDir * moveSpeed + new Vector3(0, velocity.y, 0)) * Time.deltaTime);
+    }
+
+    private void ApplyGravity()
+    {
+        if (controller.isGrounded && velocity.y < 0f)
+        {
+            velocity.y = -1f;  // 살짝 눌러 붙게
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+    }
+
+    private void Jump()
+    {
+        if (controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    // Input System 콜백
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            Jump();
+    }
+}
