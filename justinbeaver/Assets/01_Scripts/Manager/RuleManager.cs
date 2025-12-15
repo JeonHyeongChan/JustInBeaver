b2b8cfe65c4ee92ffc7613ace64aa0d8f0d8f5f6
gxvelxx <wolf2005k@naver.com>
2025-12-15 16:17:08 +0900

    <chore> 주석확인
    주석 확인

--- justinbeaver/Assets/01_Scripts/Manager/RuleManager.cs
+++ justinbeaver/Assets/01_Scripts/Manager/RuleManager.cs
@@ -14,15 +14,5 @@ public enum GameState
 
 public class RuleManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
