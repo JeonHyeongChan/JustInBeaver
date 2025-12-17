5aae80b4deee3503d353a1361eeaa445e8d7446b
gxvelxx <wolf2005k@naver.com>
2025-12-17 14:12:38 +0900

    <refactor> 상호작용 후 유령
    상호작용으로 씬 이동 후 파괴된 물체가 유령처럼 남아 어디서든 Z키가 발동하고 다시 씬으로 이동하는 버그를 없애는 코드 리팩토링

--- justinbeaver/Assets/01_Scripts/Player/BeaverInteraction/PlayerInteractDetector.cs
+++ justinbeaver/Assets/01_Scripts/Player/BeaverInteraction/PlayerInteractDetector.cs
@@ -1,8 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerInteractDetector : MonoBehaviour
 {
     public IInteractable currentTarget;
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        currentTarget = null; // 씬이 바뀔때 이전 상호작용 대상 제거 해야함
+    }
 
     private void OnTriggerEnter(Collider other)
     {
