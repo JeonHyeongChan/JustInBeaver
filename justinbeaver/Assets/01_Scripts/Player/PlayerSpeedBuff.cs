28a821be84b7c312f1a3f012f573da8b10dd9d9b
Juub0618 <dnqls1243@gmail.com>
2025-12-19 15:43:57 +0900

    <Feat> 스피드업
    <Feat> 스피드업

---
+++ justinbeaver/Assets/01_Scripts/Player/PlayerSpeedBuff.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerSpeedBuff : MonoBehaviour
+{
+    [SerializeField] private float hitSpeedMulitiplier = 1.5f;
+    [SerializeField] private float hitSpeedDuration = 3f;
+
+    private Coroutine coroutine;
+    private float multitiplier = 1f;
+    public float currentMulitiplier => multitiplier;
+
+    public void ApplyHitSpeedBoot()
+    {
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+        coroutine = StartCoroutine(CoBoost());
+    }
+
+    private IEnumerator CoBoost()
+    {
+        multitiplier = hitSpeedMulitiplier;
+        yield return new WaitForSeconds(hitSpeedDuration);
+        multitiplier = 1f;
+        coroutine = null;
+    }
+}
