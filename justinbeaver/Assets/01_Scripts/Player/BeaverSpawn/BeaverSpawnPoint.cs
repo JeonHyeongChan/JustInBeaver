67636f89560415a9cc8cbc96cb492c54941a91e4
gxvelxx <wolf2005k@naver.com>
2025-12-24 12:09:59 +0900

    <refactor> 비버 스폰 관련 리팩토링
    비버집 각 레벨별 프리펩에 스폰 위치를 추가하는 과정에서 위치를 못 받아 오는 문제가 발생에 스폰 관련 코드들 리팩토링 진행

--- justinbeaver/Assets/01_Scripts/Player/BeaverSpawn/BeaverSpawnPoint.cs
+++ justinbeaver/Assets/01_Scripts/Player/BeaverSpawn/BeaverSpawnPoint.cs
@@ -1,11 +1,36 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BeaverSpawnPoint : MonoBehaviour
 {
-    public static Transform Current;
+    private static readonly Dictionary<string, BeaverSpawnPoint> SpawnPoints =
+        new Dictionary<string, BeaverSpawnPoint>();
 
-    private void Awake()
+    private void OnEnable()
     {
-        Current = transform;
+        string sceneName = gameObject.scene.name;
+
+        SpawnPoints[sceneName] = this;
+
+        Debug.Log($"BeaverSpawnPoint for scene: {sceneName}");
+    }
+
+    private void OnDisable()
+    {
+        string sceneName = gameObject.scene.name;
+
+        if (SpawnPoints.TryGetValue(sceneName, out var point) && point == this)
+        {
+            SpawnPoints.Remove(sceneName);
+        }
+    }
+
+    public static BeaverSpawnPoint GetForCurrentScene()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        SpawnPoints.TryGetValue(sceneName, out var point);
+        return point;
     }
 }
