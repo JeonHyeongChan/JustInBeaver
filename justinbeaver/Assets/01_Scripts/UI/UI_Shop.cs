63b4335bbcd53ade44a06dc15f77fb0ef19689ee
oppa073-hub <oppa073@gmail.com>
2025-12-26 11:35:15 +0900

    <feat> 상점UI 초기상태 구현

---
+++ justinbeaver/Assets/01_Scripts/UI/UI_Shop.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class UI_Shop : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    public void OnClickUpgradeButton()
+    {
+        UIManager.Instance.ShowUpgradeUI();
+        UIManager.Instance.HideShopUI();
+    }
+    public void OnClickExitButton()
+    {
+        UIManager.Instance.HideShopUI();
+    }
+}
