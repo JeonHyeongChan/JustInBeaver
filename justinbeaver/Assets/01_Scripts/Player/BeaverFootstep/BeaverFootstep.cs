0bddd624a0661838be6ba06de1fc0b559c302131
gxvelxx <wolf2005k@naver.com>
2025-12-29 15:46:05 +0900

    <refactor> 비버 발소릭 속도 비례 구현
    인간에게 맞아서 빨리 지거나 가방이 무거워 느려졌을때 속도에 맞게 발소리 로직 수정

--- justinbeaver/Assets/01_Scripts/Player/BeaverFootstep/BeaverFootstep.cs
+++ justinbeaver/Assets/01_Scripts/Player/BeaverFootstep/BeaverFootstep.cs
@@ -2,7 +2,10 @@
 
 public class BeaverFootstep : MonoBehaviour
 {
-    public float stepInterval = 0.2f;
+    [Header("Step Timing")]
+    [SerializeField] private float minInterval = 0.15f; // 최고 속도일 때
+    [SerializeField] private float maxInterval = 0.6f; // 가방 무거울 때
+    [SerializeField] private float maxSpeedRef = 6f; // 기본 속도
 
     private float stepTimer;
     private Rigidbody rigid;
@@ -21,22 +24,31 @@ public class BeaverFootstep : MonoBehaviour
             stepTimer = 0f;
             return;
         }
+        
+        float speed = GetSpeed();
+
+        float temp = Mathf.Clamp01(speed / maxSpeedRef); // 0 ~ 1
+
+        float interval = Mathf.Lerp(maxInterval, minInterval, temp); // 빠를수록 인터벌 감소
 
         stepTimer += Time.deltaTime;
 
-        if (stepTimer >= stepInterval)
-        {
-            Debug.Log("Beaver Footstep!");
+        if (stepTimer >= interval)
+        {            
             SoundManager.Instance?.PlaySFX(SFXType.BeaverMove);
             stepTimer = 0f;
         }
     }
 
     private bool IsMovingOnGround()
+    {        
+        return controller.IsGrounded && !controller.IsRolling && GetSpeed() > 0.2f;
+    }
+
+    private float GetSpeed()
     {
         Vector3 velocity = rigid.linearVelocity;
         velocity.y = 0f;
-
-        return velocity.magnitude > 0.2f && controller.IsGrounded && !controller.IsRolling;
+        return velocity.magnitude;
     }
 }
